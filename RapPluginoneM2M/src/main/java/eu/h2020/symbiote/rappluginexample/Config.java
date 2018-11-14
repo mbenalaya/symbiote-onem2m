38c8c062e192ecfe9c1b8fc701162370a347eb89
gghabri <44877848+gghabri@users.noreply.github.com>
2018-11-14 12:10:48 +0100

    addition of key attribute and update of cseId and cseName attributes

--- RapPluginoneM2M/src/main/java/eu/h2020/symbiote/rappluginexample/Config.java
+++ RapPluginoneM2M/src/main/java/eu/h2020/symbiote/rappluginexample/Config.java
@@ -2,8 +2,8 @@ package eu.h2020.symbiote.rappluginexample;
 
 public class Config {
 	
-	public  static String originator="admin:admin";
-	public final static String key="admin";
+	public  static String originator="Cae-admin";
+	public final static String key="changeme";
 
 	
 	public final static String cseProtocol="http";
@@ -12,8 +12,8 @@ public class Config {
 	public static int csePort = 8080; 
 	
 	
-	public static String cseId = "gateway-1";
-	public static String cseName = "gateway-1";
+	public static String cseId = "server";
+	public static String cseName = "server";
 	
 	public static String contentType="application/json";
 	
@@ -22,7 +22,7 @@ public class Config {
 	public static double longitude = 1.501385;
     	public static double latitude = 43.549468;
     
-    	public static String place = "Sensinov"; 
+   	public static String place = "Sensinov"; 
     	public static String town = "City of Toulouse"; 
 
 }
